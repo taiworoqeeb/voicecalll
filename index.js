9d594017ce7659e682cf9e2dd5bdb9392f3cb44f
Abdulraqeeb taiwo <taiworoqeeb@gmail.com>
2025-02-13 11:06:01 +0100

    -a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const ngrok = require('@ngrok/ngrok');
+// const ngrok = require('@ngrok/ngrok');
 // const socketIo = require("socket.io");
 
 const app = express();
